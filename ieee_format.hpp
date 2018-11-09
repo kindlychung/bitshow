19adc62b453c008ee0da41ac6abfd00332e1c2cb
Kaiyin Zhong <kindlychung@gmail.com>
2018-11-09 15:30:10 +0100

    fix a template specialization bug

--- ieee_format.hpp
+++ ieee_format.hpp
@@ -4,12 +4,15 @@
 namespace floating_formats {
 template <typename A>
 struct format {};
+
+template <>
 struct format<double> {
     std::bitset<1> sign;
     std::bitset<11> exponent;
     std::bitset<52> mantissa;
 };
 
+template <>
 struct format<float> {
     std::bitset<1> sign;
     std::bitset<8> exponent;
